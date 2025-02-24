cfc1e60439845e5ef9e6d9a001547f2adaef3694
Etheiry <144289258+Etheiry@users.noreply.github.com>
2025-02-24 14:39:18 -0500

    inital

--- src/main/java/EmailValidator.java
+++ src/main/java/EmailValidator.java
@@ -5,10 +5,40 @@ public class EmailValidator {
     public static final char AT = '@';
 
     // Add your enumerated states after this line
+    private static final int LOCAL = 0;
+    private static final int DOMAIN = 1;
 
 
     public static boolean isEmailValid(String address){
-        // TODO: replace this line with your code
+        int count = 0;
+        int totalDomainCount = 0;
+        int currentState = LOCAL;
+        char ch;
+
+        for (int index = 0; index < address.length(); index++){
+            ch = address.charAt(index);
+
+            if (currentState == LOCAL) {
+                // if first char == dot
+                if (index == 0 && ch == DOT) {
+                    return false;
+                }
+                // If char is a dot
+                if (index > 0 && ch == DOT) {
+
+                    if (address.charAt(index + 1) == DOT || address.charAt(index + 1) == AT) {
+                        return false;
+                    }
+                    currentState = DOMAIN;
+                }
+                count++;
+            }
+
+
+        }
+
+
+
         return false;
     }
 }
